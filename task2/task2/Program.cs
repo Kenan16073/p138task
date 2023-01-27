d368d7cc52c48b8783a9adeec99b8ff515f9c09c
Kenan <kananmh@code.edu.az>
2023-01-27 04:08:24 +0400

    task

---
+++ task2/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {65,7771657,753,387,4896,5445,};
+            var max = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                if(arr[i] >max)
+                    max= arr[i];
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ task33/task33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, -8 };
+            var count = 0;
+            for (int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] < 0)
+                {
+                    count++;
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            if (count== 0)
+            {
+                Console.WriteLine("menfi eded yoxdur");
+            }
+        }
+    }
+}
